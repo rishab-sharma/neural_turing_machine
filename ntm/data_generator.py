5b741f9ead19256165960a8ff21acf2a5402522e
rishabsharma <rishabsharma@gofynd.com>
2019-03-07 14:49:41 +0530

    NTM

--- ntm/data_generator.py
+++ ntm/data_generator.py
@@ -6,40 +6,45 @@ import os
 np.random.seed(0)
 torch.manual_seed(0)
 
-length = 1000
-num_samples = 100
+class datagen():
 
-def sine_2(X, signal_freq=60.):
-    return (np.sin(2 * np.pi * (X) / signal_freq) + np.sin(4 * np.pi * (X) / signal_freq)) / 2.0
+    def __init__(self, length = 1000, num_samples = 100):
+        length = self.length
+        num_samples = self.num_samples
 
-def noisy(Y, noise_range=(-0.05, 0.05)):
-    noise = np.random.uniform(noise_range[0], noise_range[1], size=Y.shape)
-    return Y + noise
+    def sine_2(self, X, signal_freq=60.):
+        return (np.sin(2 * np.pi * (X) / signal_freq) + np.sin(4 * np.pi * (X) / signal_freq)) / 2.0
 
-def noisy_sine(length, num_samples):
-    x = []
-    for i in range(num_samples):
-        random_offset = random.randint(0, length)
-        X = np.arange(length)
-        s = sine_2(X + random_offset)
-        Y = noisy(s)
-        x.append(sample(length))
-    x = np.array(x)
-    data = x.astype('float64')
-    if not os.path.exists('./data'):
-        os.mkdir('./data')
-    torch.save(data, open('./data/traindata_noisy.pt', 'wb'))
-    print("Training Data saved in ./data/traindata_noisy.pt")
-    return x
+    def noisy(self, Y, noise_range=(-0.05, 0.05)):
+        noise = np.random.uniform(noise_range[0], noise_range[1], size=Y.shape)
+        return Y + noise
 
-def plane_sine(length, num_samples):
-    np.random.seed(2)
-    T = 20
-    x = np.empty((num_samples, length), 'int64')
-    x[:] = np.array(range(length)) + np.random.randint(-4 * T, 4 * T, N).reshape(N, 1)
-    data = np.sin(x / 1.0 / T).astype('float64')
-    if not os.path.exists('./data'):
-        os.mkdir('./data')
-    torch.save(data, open('traindata_plain.pt', 'wb'))
-    print("Training Data saved in ./data/traindata_plain.pt")
-    return x
+    def noisy_sine(self):
+        x = []
+        for i in range(self.num_samples):
+            random_offset = random.randint(0, self.length)
+            X = np.arange(self.length)
+            s = sine_2(X + random_offset)
+            Y = noisy(s)
+            x.append(sample(self.length))
+        x = np.array(x)
+        data = x.astype('float64')
+        cwd = os.getcwd()
+        if not os.path.exists('./data'):
+            os.mkdir('./data')
+        torch.save(data, open(cwd+'/traindata_noisy.pt', 'wb'))
+        print("Training Data saved in {}/traindata_noisy.pt".format(cwd))
+        return x
+
+    def plane_sine(self):
+        np.random.seed(2)
+        T = 20
+        x = np.empty((self.num_samples, self.length), 'int64')
+        x[:] = np.array(range(self.length)) + np.random.randint(-4 * T, 4 * T, N).reshape(N, 1)
+        data = np.sin(x / 1.0 / T).astype('float64')
+        if not os.path.exists('./data'):
+            os.mkdir('./data')
+        cwd = os.getcwd()
+        torch.save(data, open(cwd+'traindata_plain.pt', 'wb'))
+        print("Training Data saved in {}/traindata_plain.pt".format(cwd))
+        return x
